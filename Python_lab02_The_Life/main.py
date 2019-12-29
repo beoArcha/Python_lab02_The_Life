910b924070a6338d5afa03390a4ce741b7ba6bf4
Marcin Stefański <marcinstefanski@outlook.com>
2019-12-29 17:09:32 +0100

    argparse

--- Python_lab02_The_Life/main.py
+++ Python_lab02_The_Life/main.py
@@ -1,5 +1,17 @@
-def main():
-    pass
+import argparse
+
+
+def main() -> None:
+    args = parsing_args()
+    grid = args.grid
+    input("Press any key to end...")
+
+
+def parsing_args() -> argparse:
+    parser = argparse.ArgumentParser(description="Play the life game.")
+    parser.add_argument("--grid", "-g", help="Size of square grid", type=int, default=10, choices=range(5, 25))
+    parser.add_argument("--debug", "-de", help="Debug mode", action="store_true")
+    return parser.parse_args()
 
 
 if '__main__' == __name__:
